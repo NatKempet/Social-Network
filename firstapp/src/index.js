0879701a8e403c281de6c33769cec3dd74109f2b
vivit123 <vivitphruktayanon@gmail.com>
2023-09-14 01:32:52 +0700

    done

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './ver1/homepage.css'; //  TODO Remove this line
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
