b636907011764ce53e70c5f07d9796af9643325f
Nat <nat_kempet@cmu.ac.th>
2023-09-14 13:43:21 +0700

    added authencation

---
+++ firstapp/src/App.jsx
@@ -0,0 +1,93 @@
+import Button from './components/button';
+import './ver1/homepage.css'; //  TODO Remove this line
+import React from 'react';
+import spider from './ver1/spider.png'
+import { Auth } from './auth';
+
+const App = () => {
+  return (
+<>
+  <div className="containner">
+    <Auth />
+    <h>Home</h>
+    <ul>
+      <li>
+        <a href="#">Foryou</a>
+      </li>
+      <li>
+        <a href="#">Following</a>
+      </li>
+    </ul>
+    <div className="menu">
+      <div className="spd">
+        <img className="spider" src={spider} alt="spider" />
+      </div>
+      <div className="clickable-icon">
+        <lord-icon
+          style={{ transform: "translateY(6px)" }}
+          className="home"
+          src="https://cdn.lordicon.com/osuxyevn.json"
+          trigger="hover"
+          colors="primary:#f0f7ff"
+        />
+        <p>Home</p>
+      </div>
+      <div className="clickable-icon">
+        <lord-icon
+          style={{ transform: "translateY(6px)" }}
+          className="search"
+          src="https://cdn.lordicon.com/xfftupfv.json"
+          trigger="hover"
+          colors="primary:#f0f7ff"
+        />
+        <p>Search</p>
+      </div>
+      <div className="clickable-icon">
+        <lord-icon
+          style={{ transform: "translateY(6px)" }}
+          className="favorite"
+          src="https://cdn.lordicon.com/pnhskdva.json"
+          trigger="hover"
+          colors="primary:#f0f7ff"
+        />
+        <p>Favorite</p>
+      </div>
+      <div className="clickable-icon">
+        <lord-icon
+          style={{ transform: "translateY(6px)" }}
+          className="communities"
+          src="https://cdn.lordicon.com/bhfjfgqz.json"
+          trigger="hover"
+          colors="primary:#f0f7ff"
+        />
+        <p>Communities</p>
+      </div>
+      <div className="clickable-icon">
+        <lord-icon
+          style={{ transform: "translateY(6px)" }}
+          className="notifications"
+          src="https://cdn.lordicon.com/psnhyobz.json"
+          trigger="hover"
+          colors="primary:#f0f7ff"
+        />
+        <p>Notifications</p>
+      </div>
+      <div className="clickable-icon">
+        <lord-icon
+          style={{ transform: "translateY(6px)" }}
+          className="messages"
+          src="https://cdn.lordicon.com/hpivxauj.json"
+          trigger="hover"
+          colors="primary:#f0f7ff"
+        />
+        <p>Messages</p>
+      </div>
+      <button className="btn post">Post</button>
+    </div>
+  </div>
+</>
+
+  );
+}
+
+export default App;
