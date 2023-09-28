93dc1f940839d451e42e930dcf82f97462ae7662
Nat <nat_kempet@cmu.ac.th>
2023-09-28 15:06:41 +0700

    House

--- firstapp/src/components/tweetList.js
+++ firstapp/src/components/tweetList.js
@@ -1,17 +1,37 @@
-import React, { useContext } from 'react'
-import Tweet from './Tweet';
-import { TweetContext } from './TweetContext';
-import { db } from '../Firebase/firebase'
+import React, { useContext, useEffect, useState } from "react";
+import Tweet from "./Tweet";
+import { TweetContext } from "./TweetContext";
+import { db } from "../Firebase/firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 const TweetList = () => {
-    const [tweets, setTweet] = useContext(TweetContext);
-    return (
-        <div>
-            {tweets?.map(tweet => (
-                <Tweet title={tweet.title} content={tweet.content} likes={tweet.likes} key={tweet.id} />
-            ))}
-        </div>
-    )
-}
+  const [tweets, setTweet] = useContext(TweetContext);
+  const getTweetsCollection = collection(db, "Post");
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    const getTweets = async () => {
+      const data = await getDocs(getTweetsCollection);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setPost(filteredData);
+      console.log(post);
+    };
+    getTweets();
+  });
+  return (
+    <div>
+      {tweets?.map((tweet) => (
+        <Tweet
+          title={post.title}
+          content={post.description}
+          likes={post.like}
+          key={tweet.id}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default TweetList
+export default TweetList;
