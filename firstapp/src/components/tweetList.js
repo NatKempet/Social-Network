ac2b213c66561c4a47c32709949f6b9f5c9add7e
Ritthanupahp Sitthananun <ritthanupahp_s@cmu.ac.th>
2023-09-28 14:28:00 +0700

    Fixed merge

--- firstapp/src/components/tweetList.js
+++ firstapp/src/components/tweetList.js
@@ -15,20 +15,3 @@ const TweetList = () => {
 }
 
 export default TweetList
-
-import React, { useContext } from 'react'
-import Tweet from './Tweet';
-import { TweetContext } from './TweetContext';
-
-const TweetList = () => {
-    const [tweets, setTweet] = useContext(TweetContext);
-    return (
-        <div>
-            {tweets?.map(tweet => (
-                <Tweet title={tweet.title} content={tweet.content} likes={tweet.likes} key={tweet.id} />
-            ))}
-        </div>
-    )
-}
-
-export default TweetList
