efab7dfa785fafd7d271bce5b950b6123f43f7cb
Ritthanupahp Sitthananun <ritthanupahp_s@cmu.ac.th>
2023-10-02 14:11:42 +0700

    Implemented router

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,114 +1,25 @@
-import "./ver1/homepage.css"; //  TODO Remove this line
-import React, { useState, useEffect } from "react";
-import spider from "./ver1/spider.png";
 import { TweetProvider, TweetContext } from "./components/TweetContext";
-import TweetList from "./components/tweetList";
-import AddTweet from "./components/AddTweet";
-import DeleteButton from "./components/DeleteButton";
-import { db } from "./Firebase/firebase";
-import {
-  collection,
-  onSnapshot,
-  serverTimestamp,
-  addDoc,
-} from "firebase/firestore";
-import { Auth } from "./components/auth";
-// import Login from "./pages/Login";
+import "./App.css";
 
 // TODO make React Router work
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Missing from "./pages/Missing";
 
 const App = () => {
-
-
   return (
     <TweetProvider>
-      <>
-      <div id="containner">
-          <Auth />
-          <h>Home</h>
-          <ul>
-            <li>
-              <a href="#">For you</a>
-            </li>
-            <li>
-              <a href="#">Following</a>
-            </li>
-          </ul>
-          <div id="menu">
-            <div className="spd">
-              <img className="spider" src={spider} alt="spider" />
-            </div>
-            <div id="clickable-icon">
-              <lord-icon
-                style={{ transform: "translateY(6px)" }}
-                className="home"
-                src="https://cdn.lordicon.com/osuxyevn.json"
-                trigger="hover"
-                colors="primary:#f0f7ff"
-              />
-              <p>Home</p>
-            </div>
-            <div id="clickable-icon">
-              <lord-icon
-                style={{ transform: "translateY(6px)" }}
-                className="search"
-                src="https://cdn.lordicon.com/xfftupfv.json"
-                trigger="hover"
-                colors="primary:#f0f7ff"
-              />
-              <p>Search</p>
-            </div>
-            <div id="clickable-icon">
-              <lord-icon
-                style={{ transform: "translateY(6px)" }}
-                className="favorite"
-                src="https://cdn.lordicon.com/pnhskdva.json"
-                trigger="hover"
-                colors="primary:#f0f7ff"
-              />
-              <p>Favorite</p>
-            </div>
-            <div id="clickable-icon">
-              <lord-icon
-                style={{ transform: "translateY(6px)" }}
-                className="communities"
-                src="https://cdn.lordicon.com/bhfjfgqz.json"
-                trigger="hover"
-                colors="primary:#f0f7ff"
-              />
-              <p>Communities</p>
-            </div>
-            <div id="clickable-icon">
-              <lord-icon
-                style={{ transform: "translateY(6px)" }}
-                className="notifications"
-                src="https://cdn.lordicon.com/psnhyobz.json"
-                trigger="hover"
-                colors="primary:#f0f7ff"
-              />
-              <p>Notifications</p>
-            </div>
-            <div id="clickable-icon">
-              <lord-icon
-                style={{ transform: "translateY(6px)" }}
-                className="messages"
-                src="https://cdn.lordicon.com/hpivxauj.json"
-                trigger="hover"
-                colors="primary:#f0f7ff"
-              />
-              <p>Messages</p>
-            </div>
-          </div>
-          <DeleteButton /> {/* temporary */}
-          <div className="tweetBox">
-            <AddTweet />
-            <TweetList />
-
-          </div>
-        </div>
-      </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<Missing />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </TweetProvider>
   );
 };
---
+++ firstapp/src/pages/Home.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import "../components/homeComponents/Home.css"
+import spider from "../components/homeComponents/spider.png";
+import TweetList from "../components/tweetList";
+import AddTweet from "../components/AddTweet";
+import DeleteButton from "../components/DeleteButton";
+
+
+const App = () => {
+
+
+    return (
+        <div id="containner">
+            <h>Home</h>
+            <ul>
+                <li>
+                    <a href="#">For you</a>
+                </li>
+                <li>
+                    <a href="#">Following</a>
+                </li>
+            </ul>
+            <div id="menu">
+                <div className="spd">
+                    <img className="spider" src={spider} alt="spider" />
+                </div>
+                <div id="clickable-icon">
+                    <lord-icon
+                        style={{ transform: "translateY(6px)" }}
+                        className="home"
+                        src="https://cdn.lordicon.com/osuxyevn.json"
+                        trigger="hover"
+                        colors="primary:#f0f7ff"
+                    />
+                    <p>Home</p>
+                </div>
+                <div id="clickable-icon">
+                    <lord-icon
+                        style={{ transform: "translateY(6px)" }}
+                        className="search"
+                        src="https://cdn.lordicon.com/xfftupfv.json"
+                        trigger="hover"
+                        colors="primary:#f0f7ff"
+                    />
+                    <p>Search</p>
+                </div>
+                <div id="clickable-icon">
+                    <lord-icon
+                        style={{ transform: "translateY(6px)" }}
+                        className="favorite"
+                        src="https://cdn.lordicon.com/pnhskdva.json"
+                        trigger="hover"
+                        colors="primary:#f0f7ff"
+                    />
+                    <p>Favorite</p>
+                </div>
+                <div id="clickable-icon">
+                    <lord-icon
+                        style={{ transform: "translateY(6px)" }}
+                        className="communities"
+                        src="https://cdn.lordicon.com/bhfjfgqz.json"
+                        trigger="hover"
+                        colors="primary:#f0f7ff"
+                    />
+                    <p>Communities</p>
+                </div>
+                <div id="clickable-icon">
+                    <lord-icon
+                        style={{ transform: "translateY(6px)" }}
+                        className="notifications"
+                        src="https://cdn.lordicon.com/psnhyobz.json"
+                        trigger="hover"
+                        colors="primary:#f0f7ff"
+                    />
+                    <p>Notifications</p>
+                </div>
+                <div id="clickable-icon">
+                    <lord-icon
+                        style={{ transform: "translateY(6px)" }}
+                        className="messages"
+                        src="https://cdn.lordicon.com/hpivxauj.json"
+                        trigger="hover"
+                        colors="primary:#f0f7ff"
+                    />
+                    <p>Messages</p>
+                </div>
+            </div>
+            <DeleteButton /> {/* temporary */}
+            <div className="tweetBox">
+                <AddTweet />
+                <TweetList />
+            </div>
+        </div>
+    );
+};
+
+export default App;
