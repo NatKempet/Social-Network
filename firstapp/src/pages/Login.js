044facf6a52308e8ad3189f8f0e3df1db24b53bd
Ritthanupahp Sitthananun <ritthanupahp_s@cmu.ac.th>
2023-10-04 17:45:38 +0700

    Mew - styled login page

--- firstapp/src/pages/Login.js
+++ firstapp/src/pages/Login.js
@@ -4,19 +4,17 @@ import '../components/loginComponents/login.css'
 import AKImage from '../components/loginComponents/AK.png'
 import heartImage from '../components/loginComponents/heart.png'
 import jokerImage from '../components/loginComponents/joker.jpg'
+import joker2Image from '../components/loginComponents/Joker2.jpg'
+import heart2Image from '../components/loginComponents/heart2.png'
 import { Auth } from '../components/loginComponents/auth.jsx';
 
 
-const Login = () => {
-  const [isFlipped, setFlipped] = useState(false);
 
-  const flipCard = () => {
-    setFlipped(!isFlipped);
-  };
+const Login = () => {
 
   return (
     <>
-      <div className='background' /*style={{zIndex: '-1px'}}*/>
+      <div className='background'>
         <div id="up" />
         <div id="down" />
         <div id="left" />
@@ -26,37 +24,27 @@ const Login = () => {
         <img id="pic1" src={heartImage} />
         <img id="pic2" src={heartImage} />
         <img id="pic3" src={AKImage} />
+        <img id="pic4" src={joker2Image}/>
+        
+        
 
-        <div className={`flip-card1 ${isFlipped ? 'flipped' : ''}`} onClick={flipCard}>
-          <div className="flip-card1-inner">
-            <div className="flip-card1-front">
-              <h3 className='loginH3'>Click Me</h3>
-
+        <div id="form">
+           <h3>SIGN IN</h3>
+            <br />
+            <br />
+            <br />
+            <br />
+            <Auth />
               <h5>K</h5>
               <h6>K</h6>
-              <div className="lineup" />
-              <div className="linedown" />
-              <div className="lineleft" />
-              <div className="lineright" />
-            </div>
-            <div className="flip-card1-back">
-              <h4>SIGN IN</h4>
-              <br />
-              <br />
-              <br />
-              <br />
-
-              <Auth />
-              <h7>K</h7>
-              <h8>K</h8>
-              <div className="lineup1" />
-              <div className="linedown1" />
-              <div className="lineleft1" />
-              <div className="lineright1" />
-            </div>
-          </div>
-          <img id="joker" src={jokerImage} alt="joker" />
+              <img id="pic5" src={heart2Image}/>
+              <img id="pic6" src={heart2Image}/>
+              
+             
+              {/* <div> <Auth /> </div> */}
         </div>
+         <img id="joker" src={jokerImage} alt="joker" />
+        
       </div>
 
     </>
