e6be874aa84a85c006622161e92c9e1f5055eaf6
Ritthanupahp Sitthananun <ritthanupahp_s@cmu.ac.th>
2023-10-21 18:32:57 +0700

    Decorating LoginCard

--- firstapp/src/components/LoginCard.js
+++ firstapp/src/components/LoginCard.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import './LoginCard.css';
+import heartImage from './LoginHTML/heart.png';
+import akImage from './LoginHTML/AK.png';
+import joker from './LoginHTML/joker.jpg';
 
 const LoginCard = () => {
     const [isFlipped, setIsFlipped] = useState(false);
@@ -9,28 +12,47 @@ const LoginCard = () => {
     };
 
     return (
-        <div
-            className={`flip-card1 ${isFlipped ? 'flipped' : ''}`}
-            onClick={flipCard}
-        >
+        <>
+            <div id="up"></div>
+            <div id="down"></div>
+            <div id="left"></div>
+            <div id="right"></div>
+            <h1>K</h1>
+            <h2>K</h2>
 
-            <div className="flip-card1-inner">
-                <div className="flip-card1-front">
-                    <h3>LOGIN</h3>
-                    <div className="lineup"></div>
-                    <div className="linedown"></div>
-                    <div className="lineleft"></div>
-                    <div className="lineright"></div>
-                </div>
-                <div className="flip-card1-back">
-                    <h4>SIGN IN</h4>
-                    <div className="lineup1"></div>
-                    <div className="linedown1"></div>
-                    <div className="lineleft1"></div>
-                    <div className="lineright1"></div>
+            <img id="pic1" src={heartImage} alt="Heart" />
+            <img id="pic2" src={heartImage} alt="Heart" />
+            <img id="pic3" src={akImage} alt="AK" />
+
+
+            <div
+                className={`flip-card1 ${isFlipped ? 'flipped' : ''}`}
+                onClick={flipCard}
+            >
+
+
+                <div className="flip-card1-inner">
+                    <div className="flip-card1-front">
+                        <h3>LOGIN</h3>
+                        <div className="lineup"></div>
+                        <div className="linedown"></div>
+                        <div className="lineleft"></div>
+                        <div className="lineright"></div>
+                    </div>
+                    <div className="flip-card1-back">
+                        <h4>SIGN IN</h4>
+                        <h7>K</h7>
+                        <h8>K</h8>
+                        <div className="lineup1"></div>
+                        <div className="linedown1"></div>
+                        <div className="lineleft1"></div>
+                        <div className="lineright1"></div>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <img id="joker" src={joker} alt="joker" />
+        </>
     );
 };
 
